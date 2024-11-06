6700a1ccda84cb59dc07a5f6887e3d2ec9934220
afedyk <afedyk@comcast.net>
2024-11-05 21:15:26 -0500

    first pass at setting up application

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+const path = require('path');
+const {fileURLTOPath} = require('url');
 const express = require('express');
 const { TranslateClient, TranslateTextCommand } = require('@aws-sdk/client-translate');
 
 const app = express();
+app.use(express.static('public')); // this is added!
 app.use(express.json());
+app.use('/css', express.static('public/stylesheets'));
+
 
 const region = 'us-east-1';
 
 const translateClient = new TranslateClient({
-    region: "us-east-1",
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
 
 
@@ -42,3 +47,4 @@ const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
