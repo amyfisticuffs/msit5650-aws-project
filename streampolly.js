f3c8d7c378a34c49623920525769b4f6016e545d
Ubuntu <ubuntu@ip-172-31-38-122.ec2.internal>
2024-11-12 19:59:31 +0000

    polly streaming instead of saving to file

---
+++ streampolly.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
+const polly = new AWS.Polly();
+const app = express();
+
+// Serve the HTML file
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/stream.html');
+});
+
+app.get('/speak', (req, res) => {
+  const params = {
+    Text: "Hello, this is streamed directly from Polly!",
+    OutputFormat: "mp3",
+    VoiceId: "Joanna"
+  };
+
+  polly.synthesizeSpeech(params, (err, data) => {
+    if (err) {
+      console.error("Error synthesizing speech:", err);
+      res.status(500).send("Error synthesizing speech");
+    } else if (data && data.AudioStream instanceof Buffer) {
+      res.setHeader('Content-Type', 'audio/mpeg');
+      res.send(data.AudioStream);
+    }
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Server running on http://localhost:8080');
+});
+
